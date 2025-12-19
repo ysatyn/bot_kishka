03fd6f0f82345b63d0934e0dd5384d287af16999
ysatyn <nadporozhskiyivan@gmail.com>
2025-12-19 21:11:49 +0300

    Инициализация и зависимости

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-z = 67
-print(z)
+from telebot.async_telebot import AsyncTeleBot
+import asyncio
+
+bot = AsyncTeleBot("ysatyn")
+
+async def main():
+    await bot.infinity_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main)
